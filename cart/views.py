8c395f3ab6d6fb9fc7d625985174b31982d6e004
Paweł Florczak <pawel.florczak87@gmail.com>
2021-02-24 21:16:13 +0100

    2021.02.24

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from myshop.shop.models import Product
+from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -14,6 +14,19 @@ def cart_add(request, product_id):
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
     return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
+    return render(
+        request,
+        'cart/detail.html',
+        {
+            'cart': cart
+        }
+    )
 
 
 def cart_remove(request, product_id):
